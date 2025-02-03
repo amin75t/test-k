0ba5b33cdd8373ed846df90b61256fbf255ab729
amin75t <167482540+amin75t-work@users.noreply.github.com>
2025-02-03 19:32:36 +0330

    init1

--- src/screen/app_screen.jsx
+++ src/screen/app_screen.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const AppScreen = () => {
   useEffect(() => {
-    window.location.href = "https://www.amin.com/app";
+    window.location.href = "https://test-k-beige.vercel.app/app";
   }, []);
 
   return <h2>Redirecting...</h2>;
